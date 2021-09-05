3fff88eaa601fb9de2672a9329cc9b9d1eb29224
Mohammad Asif <masif0023@gmail.com>
2021-09-05 13:20:32 +0530

    React project using API

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import './src/Components/Header.css';
+<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"></link>
+
+export default function Header() {
+	
+    return (
+		
+        <div>
+          	<nav class="navigation">
+			  <h1> git users</h1>
+		</nav>
+		
+        </div>
+		
+    )
+}
